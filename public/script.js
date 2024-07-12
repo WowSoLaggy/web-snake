2e5b674f2b2517a9f2a628d29aa0464c447f1aad
Anton Egorov <aegorov@aligntech.com>
2024-07-12 13:41:12 +0300

    Removed time

--- public/script.js
+++ public/script.js
@@ -223,10 +223,7 @@ async function loadScores() {
         const day = String(date.getDate()).padStart(2, '0');
         const month = String(date.getMonth() + 1).padStart(2, '0'); // Месяцы от 0 до 11
         const year = date.getFullYear();
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-        const formattedDate = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+        const formattedDate = `${day}-${month}-${year}`;
         return `
             <tr>
                 <td>${score.name}</td>
